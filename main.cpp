b4123a9e3422330848000e3e59834ae629cf0621
KimSeungHan <rlatmdgks4@naver.com>
2026-02-24 15:21:01 +0900

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a;
+	cin >> a;
+	for (int i = 0; i < a; i++) {
+		cout << "Go Home" << endl;
+	}
+
+	cout << "하지만 못간다." << endl;
+}
